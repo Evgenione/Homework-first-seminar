ea93e946ac09caafc6469e0b9a2760c460825eb7
Evgenione <fallangel07021989@gmail.com>
2023-02-16 01:32:52 +0300

    add exercise1

---
+++ exercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите чосло a"); 
+int a = int.Parse(Console.ReadLine())!;
+Console.WriteLine("введите чосло b"); 
+int b = int.Parse(Console.ReadLine())!;
+Console.WriteLine("введите чосло c"); 
+int c = int.Parse(Console.ReadLine())!;
+int max = a;
+
+if(b > max) max = b;
+
+if(c > max) max = c;
+
+Console.WriteLine (max);
