787a9f9831052ee5e28461969058179b553d6dfc
Evgenione <fallangel07021989@gmail.com>
2023-02-16 09:38:30 +0300

    add exercise3

---
+++ exercise3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+Console.Write("A-> ");
+int a = Int32.Parse(Console.ReadLine());
+int b;
+while (true)
+{
+    Console.Write ("B-> ");
+    b = Int32.Parse(Console.ReadLine());
+    if (a<=b)
+        break;
+}
+for (int i = a; i <= b; i++)
+    if (i % 2 == 0 && i % 2 == 0)
+        Console.WriteLine (i);
+    Console.ReadLine();
